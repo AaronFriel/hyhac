67710ae59642f38aeee6037cee51300c9783a0a2
Aaron Friel <mayreply@aaronfriel.com>
2013-05-06 18:57:14 -0500

    Split out test case for create and removing spaces

--- test/Test/HyperDex/Internal.hs
+++ test/Test/HyperDex/Internal.hs
@@ -6,8 +6,16 @@ import Test.Framework.Providers.HUnit
 import Test.HUnit hiding (Test)
 
 import Database.HyperDex.Internal
+
+import Data.Int
 import Foreign.C
 import Foreign.Ptr
+
+defaultHost :: String
+defaultHost = "127.0.0.1"
+
+defaultPort :: Int16
+defaultPort = 1982
 
 defaultSpaceName :: String
 defaultSpaceName = "profiles"
@@ -27,18 +35,33 @@ defaultSpace =
   \subspace first, last                     \n\
   \subspace profile_views"
 
-canCreateAndRemoveSpaces :: Test
-canCreateAndRemoveSpaces = testCase "Can create and remove spaces" $ do
-  withCString "127.0.0.1" $ \host -> do
-    client <- hyperclientCreate host 1982
+withDefaultHost :: (HyperclientPtr -> IO a) -> IO a
+withDefaultHost f =
+  withCString defaultHost $ \host -> do
+    client <- hyperclientCreate host defaultPort
+    res <- f client
+    hyperclientDestroy client
+    return res
+
+canCreateSpace :: Test
+canCreateSpace = testCase "Can create a space" $ do
+  withDefaultHost $ \client -> do
     withCString defaultSpace $ \space -> do
       addSpaceResult <- hyperclientAddSpace client space
       assertEqual "Add space: " HyperclientSuccess addSpaceResult
+
+
+canRemoveSpace :: Test
+canRemoveSpace = testCase "Can remove a space" $ do
+  withDefaultHost $ \client -> do
     withCString defaultSpaceName $ \spaceName -> do
       removeSpaceResult <- hyperclientRemoveSpace client spaceName
       assertEqual "Remove space: " HyperclientSuccess removeSpaceResult
 
+canCreateAndRemoveSpaces :: Test
+canCreateAndRemoveSpaces = testGroup "Can create and remove space" [ canCreateSpace, canRemoveSpace ]
+
 internalTests :: Test
 internalTests = testGroup "Internal API Tests"
-                  [   canCreateAndRemoveSpaces
+                  [ canCreateAndRemoveSpaces
                   ]
